951e567dc69fac26c48c7cbccb0abb9f10805a31
Alvin Aggrey <40688105+AlvinAggrey@users.noreply.github.com>
2022-12-11 19:42:31 +0000

    Added and revamped selection methods
    *stochastic
    *revamped roulette

--- TowerDefense 2.0 - Student/TowerDefense/SelectionMethods.cpp
+++ TowerDefense 2.0 - Student/TowerDefense/SelectionMethods.cpp
@@ -1,7 +1,8 @@
 #include "SelectionMethods.h"
 
 
-std::vector<Chromosome> RankChroms(std::vector<Chromosome>* chroms)
+
+std::vector<Chromosome> GASelection::RankChroms(std::vector<Chromosome>* chroms)
 {
     std::vector<Chromosome> rankedList;
     std::vector<Chromosome> tempList = *chroms;
@@ -31,7 +32,7 @@ std::vector<Chromosome> RankChroms(std::vector<Chromosome>* chroms)
     return rankedList;
 }
 //ranks generation, selects n from ranked list from the fittest, returns copies of selected individuals
-std::vector<Chromosome> RankedSelection(std::vector<Chromosome>* gen, int selectNum, int parentNum)
+std::vector<Chromosome> GASelection::RankedSelection(std::vector<Chromosome>* gen, int selectNum, int parentNum)
 {
 
     std::vector<Chromosome> rankedList = RankChroms(gen);
@@ -41,7 +42,7 @@ std::vector<Chromosome> RankedSelection(std::vector<Chromosome>* gen, int select
     if (selectNum - 1 > rankedList.size())
     {
         std::cout << "Generation is too small to select this many indviduals." << std::endl;
-        selectNum = rankedList.size();
+        selectNum = (int)rankedList.size();
     }
     for (int i = 0; i < selectNum; i++)
     {
@@ -66,7 +67,7 @@ std::vector<Chromosome> RankedSelection(std::vector<Chromosome>* gen, int select
     return matingPool;
 }
 //std::random_device rd;
-Chromosome onepointCrossOver(Chromosome parent1, Chromosome parent2)
+Chromosome GASelection::onepointCrossOver(Chromosome parent1, Chromosome parent2)
 {
     Chromosome child = parent1;
     child.m_fitness = 1;
@@ -82,7 +83,7 @@ Chromosome onepointCrossOver(Chromosome parent1, Chromosome parent2)
 }
 
 //split generation into bands select n from each band
-std::vector<Chromosome> LinearRankedSelection(std::vector<Chromosome>* gen, unsigned int seed, std::initializer_list<int> bandDists)
+std::vector<Chromosome> GASelection::LinearRankedSelection(std::vector<Chromosome>* gen, unsigned int seed, std::initializer_list<int> bandDists)
 {
     std::vector<Chromosome> rankedList = RankChroms(gen);
     std::vector<Chromosome> matingPool;
@@ -109,7 +110,7 @@ std::vector<Chromosome> LinearRankedSelection(std::vector<Chromosome>* gen, unsi
 }
 
 //adds indivduals to returned mating pool - linear ranked selection
-std::vector<Chromosome> ElitistSelection(std::vector<Chromosome>* gen, unsigned int seed, std::vector<Chromosome> indivs, std::initializer_list<int> bandDists)
+std::vector<Chromosome> GASelection::ElitistSelection(std::vector<Chromosome>* gen, unsigned int seed, std::vector<Chromosome> indivs, std::initializer_list<int> bandDists)
 {
     std::vector<Chromosome> matingPool = LinearRankedSelection(gen, seed, bandDists);
     if (indivs.size() != 0)
@@ -122,13 +123,13 @@ std::vector<Chromosome> ElitistSelection(std::vector<Chromosome>* gen, unsigned 
     return matingPool;
 }
 
-std::vector<Chromosome> TournamentSelection(std::vector<Chromosome>* gen, unsigned int seed, int matingPoolSize, int tournySize)
+std::vector<Chromosome> GASelection::TournamentSelection(std::vector<Chromosome>* gen, unsigned int seed, int matingPoolSize, int tournySize)
 {
     std::vector<Chromosome> matingPool;
     std::vector<Chromosome> roster = *gen;
     std::vector<Chromosome> tournament;
     std::mt19937 mt(seed);
-    std::uniform_int_distribution<int> rosterIndex(0, roster.size() - 1);
+    std::uniform_int_distribution<int> rosterIndex(0, (int)roster.size() - 1);
 
     int randNum;
 
@@ -144,5 +145,95 @@ std::vector<Chromosome> TournamentSelection(std::vector<Chromosome>* gen, unsign
         matingPool.push_back(RankChroms(&tournament)[0]);
         tournament.clear();
     }
+    return matingPool;
+}
+
+//puts individuals on a wheel and spins it, whatever the picker falls on joins the mating pool
+std::vector<Chromosome> GASelection::RouletteSelection(std::vector<Chromosome>* gen, unsigned int seed, int parentNum)
+{
+    float totalFitness = 0;
+    for (Chromosome indiv : *gen)
+    {
+        totalFitness += indiv.m_fitness;
+    }
+
+    float pickerSpot;
+    std::mt19937 mt(seed);
+    std::uniform_real_distribution<float> randSpot(0, totalFitness);
+
+    std::vector<Chromosome> wheel = *gen;
+    wheel = RankChroms(&wheel);
+    std::vector<Chromosome> matingPool;
+
+    float spinDistance;
+    for (int i = 0; i < parentNum; i++)
+    {
+        pickerSpot = randSpot(mt);
+        spinDistance = pickerSpot;
+        for (Chromosome indivSlot : wheel)
+        {
+            if (spinDistance >= 0 && spinDistance <= indivSlot.m_fitness)
+            {
+                matingPool.push_back(indivSlot);
+                break;
+            }
+            spinDistance -= indivSlot.m_fitness;
+        }
+    }
+
+    return matingPool;
+}
+
+//march around wheel a distance a set number of times add whatever the picker lands on each time
+std::vector<Chromosome> GASelection::StochasticSamplingSelection(std::vector<Chromosome>* gen, unsigned int seed, int parentNum)
+{
+    float totalFitness = 0;
+    for (Chromosome indiv : *gen)
+    {
+        totalFitness += indiv.m_fitness;
+    }
+
+    std::vector<Chromosome> wheel = *gen;
+    wheel = RankChroms(&wheel);
+    std::vector<Chromosome> matingPool;
+
+    float pickerSpot = 0;
+    int index = 0;
+    float spinDistance = 0;
+    int curPoolSize;
+    float march = totalFitness/(float)wheel.size(); //step = total fitness / size of mating pool 
+    for (int i = 0; i < parentNum; i++)
+    {
+        if (i == 0)
+        {
+            std::mt19937 mt(seed);
+            std::uniform_real_distribution<float> randSpot(0, march);
+            pickerSpot = randSpot(mt);
+        }
+        else
+        {
+            pickerSpot += march * (i + 1);
+        }
+
+        spinDistance = pickerSpot;
+        curPoolSize = (int)matingPool.size();
+
+        while (matingPool.size() < curPoolSize + 1)
+        {
+            if (index >= wheel.size())
+            {
+                index = 0;
+            }
+            Chromosome indivSlot = wheel[index];
+            if (spinDistance >= 0 && spinDistance <= indivSlot.m_fitness)
+            {
+                matingPool.push_back(indivSlot);
+                break;
+            }
+            spinDistance -= indivSlot.m_fitness;
+            index++;
+        }
+    }
+
     return matingPool;
 }
--- TowerDefense 2.0 - Student/TowerDefense/SelectionMethods.h
+++ TowerDefense 2.0 - Student/TowerDefense/SelectionMethods.h
@@ -5,10 +5,16 @@
 #include <initializer_list>
 #include <vector>
 
-std::vector<Chromosome> RankChroms(std::vector<Chromosome>* chroms);
-std::vector<Chromosome> RankedSelection(std::vector<Chromosome>* gen, int selectNum, int parentNum);
-Chromosome onepointCrossOver(Chromosome parent1, Chromosome parent2);
-std::vector<Chromosome> LinearRankedSelection(std::vector<Chromosome>* gen, unsigned int seed, std::initializer_list<int> bandDists);
-std::vector<Chromosome> ElitistSelection(std::vector<Chromosome>* gen, unsigned int seed, std::vector<Chromosome> indivs, std::initializer_list<int> bandDists);
-std::vector<Chromosome> TournamentSelection(std::vector<Chromosome>* gen, unsigned int seed, int matingPoolSize, int tournySize)
+namespace GASelection
+{
+	std::vector<Chromosome> RankChroms(std::vector<Chromosome>* chroms);
+	std::vector<Chromosome> RankedSelection(std::vector<Chromosome>* gen, int selectNum, int parentNum);
+	Chromosome onepointCrossOver(Chromosome parent1, Chromosome parent2);
+	std::vector<Chromosome> LinearRankedSelection(std::vector<Chromosome>* gen, unsigned int seed, std::initializer_list<int> bandDists);
+	std::vector<Chromosome> ElitistSelection(std::vector<Chromosome>* gen, unsigned int seed, std::vector<Chromosome> indivs, std::initializer_list<int> bandDists);
+	std::vector<Chromosome> TournamentSelection(std::vector<Chromosome>* gen, unsigned int seed, int matingPoolSize, int tournySize);
+
+	std::vector<Chromosome> StochasticSamplingSelection(std::vector<Chromosome>* gen, unsigned int seed, int parentNum);
+	std::vector<Chromosome> RouletteSelection(std::vector<Chromosome>* gen, unsigned int seed, int parentNum);
+}
 
